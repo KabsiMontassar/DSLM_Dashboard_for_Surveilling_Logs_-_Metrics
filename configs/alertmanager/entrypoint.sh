4eb8d08b7b971eefaf32a70f19e3535a431b0415
montassar.kebsi <kebsimontassar@gmail.com>
2025-09-15 22:14:40 +0100

    Switch alertmanager entrypoint from bash to sh

--- configs/alertmanager/entrypoint.sh
+++ configs/alertmanager/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Substitute environment variables in alertmanager config
 envsubst < /etc/alertmanager/alertmanager.yml > /etc/alertmanager/alertmanager-substituted.yml
 exec /bin/alertmanager --config.file=/etc/alertmanager/alertmanager-substituted.yml --storage.path=/alertmanager
